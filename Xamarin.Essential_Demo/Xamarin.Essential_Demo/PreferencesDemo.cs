687f84f09c3b772bc007fb50e7203ff9e2acb8c5
WangWesley <wangshifu46@outlook.com>
2019-03-17 14:58:05 +0100

    Preferences

--- Xamarin.Essential_Demo/Xamarin.Essential_Demo/PreferencesDemo.cs
+++ Xamarin.Essential_Demo/Xamarin.Essential_Demo/PreferencesDemo.cs
@@ -9,156 +9,242 @@ using Xamarin.Essentials;
 
 namespace Xamarin.Essential_Demo
 {
-	public class PreferencesDemo : ContentPage
+    //public class PreferencesDemo : ContentPage
+    //   {
+    //       private string content;
+    //       private string key;
+    //       private EntryCell entry_content;
+    //       private EntryCell entry_key;
+    //       private Button saveWithKey;
+    //       private Button saveWithoutKey;
+    //       private Button retrieveWithKey;
+    //       private Button retrieveWithoutKey;
+    //       private Button removeKey;
+    //       private Button removeAllKeys;
+    //       private Label result;
+    //       public PreferencesDemo()
+    //       {
+    //           entry_content = new EntryCell { Label = "Content you want to save:", Placeholder = "Enter value here" };
+    //           entry_key = new EntryCell { Label = "Key", Placeholder = "not neccessarily needed" };
+
+    //           saveWithKey = new Button { Text = "Save with key" };
+    //           saveWithKey.Clicked += SaveWithKey_ClickedAsync;
+
+    //           saveWithoutKey = new Button { Text = "Save without key" };
+    //           saveWithoutKey.Clicked += SaveWithoutKey_Clicked;
+
+    //           retrieveWithKey = new Button { Text = "Retrieve with key" };
+    //           retrieveWithKey.Clicked += RetrieveWithKey_Clicked;
+
+    //           retrieveWithoutKey = new Button { Text = "Retrieve without key" };
+    //           retrieveWithoutKey.Clicked += RetrieveWithoutKey_Clicked;
+
+    //           removeKey = new Button { Text = "Remove key" };
+    //           removeKey.Clicked += RemoveKey_Clicked;
+
+    //           removeAllKeys = new Button { Text = "Remove all keys" };
+    //           removeAllKeys.Clicked += RemoveAllKeys_Clicked;
+
+    //           result = new Label();
+    //           TableView tableView = new TableView
+    //           {
+    //               Intent = TableIntent.Form,
+    //               Root = new TableRoot
+    //               {
+    //                   new TableSection
+    //                   {
+    //                       entry_content, entry_key
+    //                   }
+    //               }
+    //           };
+
+    //           Content = new StackLayout
+    //           {
+    //               Children = {
+    //                   new Label { Text = "This is a Secure Stroage Demo." },tableView,saveWithKey,saveWithoutKey,retrieveWithKey,retrieveWithoutKey,removeKey,removeAllKeys,result
+    //               }
+    //           };
+    //       }
+
+    //       private void RemoveAllKeys_Clicked(object sender, EventArgs e)
+    //       {
+    //           Preferences.Clear();
+    //           this.result.Text = "remove all keys: True";
+    //       }
+
+    //       private void RemoveKey_Clicked(object sender, EventArgs e)
+    //       {
+    //           string key = entry_key.Text;
+    //           bool result = false;
+    //           if (!string.IsNullOrWhiteSpace(key))
+    //           {
+    //               Preferences.Remove(key);
+    //               this.result.Text = "remove key: " + result.ToString();
+    //           }
+    //           else
+    //           {
+    //               this.result.Text = "remove key: " + result.ToString();
+    //           }
+    //       }
+
+    //       private void RetrieveWithoutKey_Clicked(object sender, EventArgs e)
+    //       {
+    //           try
+    //           {
+    //               var value = Preferences.Get(key, "The value for this key does not exist.");
+    //               result.Text = value;
+    //           }
+    //           catch (Exception ex)
+    //           {
+    //               result.Text = ex.ToString();
+    //           }
+    //       }
+
+    //       private void RetrieveWithKey_Clicked(object sender, EventArgs e)
+    //       {
+    //           string key = entry_key.Text;
+    //           if (!string.IsNullOrWhiteSpace(key))
+    //           {
+    //               try
+    //               {
+    //                   var value = Preferences.Get(key, "The value for this key does not exist.");
+    //                   result.Text = value;
+    //               }
+    //               catch (Exception ex)
+    //               {
+    //                   result.Text = ex.ToString();
+    //               }
+    //           }
+    //       }
+
+    //       private void SaveWithoutKey_Clicked(object sender, EventArgs e)
+    //       {
+    //           string content = this.entry_content.Text;
+    //           // generate a random string.
+    //           string key = Path.GetRandomFileName();
+    //           key = key.Replace(".", "");
+
+    //           if (!string.IsNullOrWhiteSpace(content) && !string.IsNullOrWhiteSpace(key))
+    //           {
+    //               try
+    //               {
+    //                   Preferences.Set(key, content);
+    //                   result.Text = "Saving succeeded!";
+    //               }
+    //               catch (Exception ex)
+    //               {
+    //                   result.Text = ex.ToString();
+    //               }
+    //               this.content = content;
+    //               this.key = key;
+    //           }
+    //       }
+
+    //       private void SaveWithKey_ClickedAsync(object sender, EventArgs e)
+    //       {
+    //           string content = this.entry_content.Text;
+    //           string key = this.entry_key.Text;
+
+    //           if (!string.IsNullOrWhiteSpace(content) && !string.IsNullOrWhiteSpace(key))
+    //           {
+    //               try
+    //               {
+    //                   Preferences.Set(key, content);
+    //                   result.Text = "Saving succeeded!";
+    //               }
+    //               catch (Exception ex)
+    //               {
+    //                   result.Text = ex.ToString();
+    //               }
+    //               this.content = content;
+    //               this.key = key;
+    //           }
+    //       }
+    //}
+    class PreferencesDemo : ContentPage
     {
-        private string content;
-        private string key;
-        private EntryCell entry_content;
-        private EntryCell entry_key;
-        private Button saveWithKey;
-        private Button saveWithoutKey;
-        private Button retrieveWithKey;
-        private Button retrieveWithoutKey;
-        private Button removeKey;
-        private Button removeAllKeys;
-        private Label result;
+        Label header;
+        Slider slider;
+        Switch switcher;
+        Entry text;
+        DatePicker datePicker;
+
         public PreferencesDemo()
         {
-            entry_content = new EntryCell { Label = "Content you want to save:", Placeholder = "Enter value here" };
-            entry_key = new EntryCell { Label = "Key", Placeholder = "not neccessarily needed" };
-
-            saveWithKey = new Button { Text = "Save with key" };
-            saveWithKey.Clicked += SaveWithKey_ClickedAsync;
-
-            saveWithoutKey = new Button { Text = "Save without key" };
-            saveWithoutKey.Clicked += SaveWithoutKey_Clicked;
-
-            retrieveWithKey = new Button { Text = "Retrieve with key" };
-            retrieveWithKey.Clicked += RetrieveWithKey_Clicked;
-
-            retrieveWithoutKey = new Button { Text = "Retrieve without key" };
-            retrieveWithoutKey.Clicked += RetrieveWithoutKey_Clicked;
-
-            removeKey = new Button { Text = "Remove key" };
-            removeKey.Clicked += RemoveKey_Clicked;
-
-            removeAllKeys = new Button { Text = "Remove all keys" };
-            removeAllKeys.Clicked += RemoveAllKeys_Clicked;
-
-            result = new Label();
-            TableView tableView = new TableView
+            header = new Label
             {
-                Intent = TableIntent.Form,
-                Root = new TableRoot
-                {
-                    new TableSection
-                    {
-                        entry_content, entry_key
-                    }
-                }
+                Text = "Preferences",
+                FontSize = 50,
+                FontAttributes = FontAttributes.Bold,
+                HorizontalOptions = LayoutOptions.Center
             };
 
-            Content = new StackLayout
+            slider = new Slider
             {
-                Children = {
-                    new Label { Text = "This is a Secure Stroage Demo." },tableView,saveWithKey,saveWithoutKey,retrieveWithKey,retrieveWithoutKey,removeKey,removeAllKeys,result
+                Minimum = 0,
+                Maximum = 1,
+                Value = Preferences.Get("SliderValue", 0.5),
+                MinimumTrackColor = Color.Pink
+            };
+            slider.ValueChanged += OnSliderValueChanged;
+
+            switcher = new Switch
+            {
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.CenterAndExpand,
+                IsToggled = Preferences.Get("IsToggle", false)
+            };
+            switcher.Toggled += OnSwitcherToggled;
+
+            text = new Entry
+            {
+                Keyboard = Keyboard.Text,
+                Placeholder = "Enter text",
+                VerticalOptions = LayoutOptions.CenterAndExpand,
+                Text = Preferences.Get("EntryText", "")
+            };
+            text.TextChanged += OnEntryTextChanged;
+
+            datePicker = new DatePicker
+            {
+                Format = "D",
+                VerticalOptions = LayoutOptions.Start,
+                Date = Preferences.Get("Date", new DateTime(2008, 6, 1))
+            };
+            datePicker.DateSelected += DatePicker_DateSelected;
+
+            // Build the page.
+            this.Content = new StackLayout
+            {
+                Children =
+                {
+                    header,
+                    slider,
+                    switcher,
+                    text,
+                    datePicker
                 }
             };
         }
 
-        private void RemoveAllKeys_Clicked(object sender, EventArgs e)
+        void OnSwitcherToggled(object sender, ToggledEventArgs e)
         {
-            Preferences.Clear();
-            this.result.Text = "remove all keys: True";
+            Preferences.Set("IsToggle", e.Value);
         }
 
-        private void RemoveKey_Clicked(object sender, EventArgs e)
+        void OnSliderValueChanged(object sender, ValueChangedEventArgs e)
         {
-            string key = entry_key.Text;
-            bool result = false;
-            if (!string.IsNullOrWhiteSpace(key))
-            {
-                Preferences.Remove(key);
-                this.result.Text = "remove key: " + result.ToString();
-            }
-            else
-            {
-                this.result.Text = "remove key: " + result.ToString();
-            }
+            Preferences.Set("SliderValue", e.NewValue);
         }
 
-        private void RetrieveWithoutKey_Clicked(object sender, EventArgs e)
+        void OnEntryTextChanged(object sender, TextChangedEventArgs e)
         {
-            try
-            {
-                var value = Preferences.Get(key, "The value for this key does not exist.");
-                result.Text = value;
-            }
-            catch (Exception ex)
-            {
-                result.Text = ex.ToString();
-            }
+            Preferences.Set("EntryText", e.NewTextValue);
         }
 
-        private void RetrieveWithKey_Clicked(object sender, EventArgs e)
+        private void DatePicker_DateSelected(object sender, DateChangedEventArgs e)
         {
-            string key = entry_key.Text;
-            if (!string.IsNullOrWhiteSpace(key))
-            {
-                try
-                {
-                    var value = Preferences.Get(key, "The value for this key does not exist.");
-                    result.Text = value;
-                }
-                catch (Exception ex)
-                {
-                    result.Text = ex.ToString();
-                }
-            }
+            Preferences.Set("Date", e.NewDate.Date);
         }
-
-        private void SaveWithoutKey_Clicked(object sender, EventArgs e)
-        {
-            string content = this.entry_content.Text;
-            // generate a random string.
-            string key = Path.GetRandomFileName();
-            key = key.Replace(".", "");
-
-            if (!string.IsNullOrWhiteSpace(content) && !string.IsNullOrWhiteSpace(key))
-            {
-                try
-                {
-                    Preferences.Set(key, content);
-                    result.Text = "Saving succeeded!";
-                }
-                catch (Exception ex)
-                {
-                    result.Text = ex.ToString();
-                }
-                this.content = content;
-                this.key = key;
-            }
-        }
-
-        private void SaveWithKey_ClickedAsync(object sender, EventArgs e)
-        {
-            string content = this.entry_content.Text;
-            string key = this.entry_key.Text;
-
-            if (!string.IsNullOrWhiteSpace(content) && !string.IsNullOrWhiteSpace(key))
-            {
-                try
-                {
-                    Preferences.Set(key, content);
-                    result.Text = "Saving succeeded!";
-                }
-                catch (Exception ex)
-                {
-                    result.Text = ex.ToString();
-                }
-                this.content = content;
-                this.key = key;
-            }
-        }
-	}
+    }
 }
